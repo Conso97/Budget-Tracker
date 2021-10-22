6f58fdd618f0fc436132f56aafba2e7dde32ad0e
Consolata Njeri <conso4u@gmail.com>
2021-10-22 13:05:46 -0700

    fixed spelling bug in service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -15,7 +15,7 @@ const DATA_CACHE_NAME = "data-cache-v1";
 //install
 self.addEventListener("install", function(evt) {
     evt.waitUntil(
-        cache.open(CACHE_NAME).then(cache => {
+        caches.open(CACHE_NAME).then(cache => {
             console.log("Your files were pre-cached successfully!");
             return cache.addAll(FILES_TO_CACHE);
         })
